0d6cc359be570ce0badf29b14c882477eae3ef09
DroysterZ <55192980+DroysterZ@users.noreply.github.com>
2026-06-26 18:02:52 -0300

    Add files via upload

--- js/data/weapons/bow/bow.tiered.js
+++ js/data/weapons/bow/bow.tiered.js
@@ -4,149 +4,318 @@
 
 export const BOW_TIERED = [
 	{
+		id: 'bow_t0',
+		class: 'bow',
+		name: 'Shortbow',
+		tier: 0,
+		projectiles: [
+			{
+				damage: 17.5,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 7.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
 		id: 'bow_t1',
 		class: 'bow',
-		name: 'bow_t1',
-		tier: 11,
+		name: 'Reinforced Bow',
+		tier: 1,
+		projectiles: [
+			{
+				damage: 22.5,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 7.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t2',
+		class: 'bow',
+		name: 'Hardwood Bow',
+		tier: 2,
+		projectiles: [
+			{
+				damage: 27.5,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 10,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t3',
+		class: 'bow',
+		name: 'Greywood Bow',
+		tier: 3,
+		projectiles: [
+			{
+				damage: 32.5,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 10,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t4',
+		class: 'bow',
+		name: 'Ironwood Bow',
+		tier: 4,
+		projectiles: [
+			{
+				damage: 40,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 17.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t5',
+		class: 'bow',
+		name: 'Fire Bow',
+		tier: 5,
+		projectiles: [
+			{
+				damage: 50,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 17.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t6',
+		class: 'bow',
+		name: 'Sharpshooter Bow',
+		tier: 6,
+		projectiles: [
+			{
+				damage: 60,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 17.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t7',
+		class: 'bow',
+		name: 'Redwood Bow',
+		tier: 7,
+		projectiles: [
+			{
+				damage: 70,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 25,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t8',
+		class: 'bow',
+		name: 'Golden Bow',
+		tier: 8,
+		projectiles: [
+			{
+				damage: 90,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 25,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t9',
+		class: 'bow',
+		name: 'Verdant Bow',
+		tier: 9,
 		projectiles: [
 			{
 				damage: 100,
 				fireRate: 100,
-				shots: 3,
+				shots: 1,
 				armorPiercing: false
 			},
+			{
+				damage: 25,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
 		],
-		canEquip: ['archer', 'huntress'],
+		canEquip: ['archer', 'huntress', 'bard'],
+	},
+	{
+		id: 'bow_t10',
+		class: 'bow',
+		name: 'Bow of Fey Magic',
+		tier: 10,
+		projectiles: [
+			{
+				damage: 110,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 30,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
+		],
+		canEquip: ['archer', 'huntress', 'bard'],
 	},
 	{
 		id: 'bow_t11',
 		class: 'bow',
-		name: 'Leaf Bow',
+		name: 'Bow of Innocent Blood',
 		tier: 11,
 		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
+			{
+				damage: 120,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 30,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
 		],
-		canEquip: ['archer', 'huntress'],
+		canEquip: ['archer', 'huntress', 'bard'],
 	},
 	{
-		id: 'bow_t11',
+		id: 'bow_t12',
 		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
+		name: 'Bow of Covert Havens',
+		tier: 12,
 		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
+			{
+				damage: 130,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 30,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
 		],
-		canEquip: ['archer', 'huntress'],
+		canEquip: ['archer', 'huntress', 'bard'],
 	},
 	{
-		id: 'bow_t11',
+		id: 'bow_t13',
 		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
+		name: 'Bow of Mystical Energy',
+		tier: 13,
 		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
+			{
+				damage: 140,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 40,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
 		],
-		canEquip: ['archer', 'huntress'],
+		canEquip: ['archer', 'huntress', 'bard'],
 	},
 	{
-		id: 'bow_t11',
+		id: 'bow_t14',
 		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
+		name: 'Bow of Deep Enchantment',
+		tier: 14,
 		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
+			{
+				damage: 150,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+			{
+				damage: 40,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: true
+			}
 		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-	{
-		id: 'bow_t11',
-		class: 'bow',
-		name: 'Leaf Bow',
-		tier: 11,
-		projectiles: [
-			{ damage: 165, fireRate: 100, shots: 1, armorPiercing: false },
-		],
-		canEquip: ['archer', 'huntress'],
-	},
-
+		canEquip: ['archer', 'huntress', 'bard'],
+	}
 ];
