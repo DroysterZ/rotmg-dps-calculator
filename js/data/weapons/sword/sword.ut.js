7592a4f7a7bb7577515e48250051adc2bc4c6662
DroysterZ <55192980+DroysterZ@users.noreply.github.com>
2026-06-30 14:43:53 -0300

    Add files via upload

---
+++ js/data/weapons/sword/sword.ut.js
@@ -0,0 +1,284 @@
+// ─────────────────────────────────────────────
+//  Weapons › Sword › Tiered
+// ─────────────────────────────────────────────
+
+export const SWORD_TIERED = [
+	{
+		id: 'sword_ut1',
+		class: 'sword',
+		name: 'MMace MMurderer',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 277.5,
+				fireRate: 40,
+				shots: 2,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut2',
+		class: 'sword',
+		name: 'FFabulous FFlail',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 333,
+				fireRate: 40,
+				shots: 2,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut3',
+		class: 'sword',
+		name: 'Vinesword',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 240,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut4',
+		class: 'sword',
+		name: 'Crystal Sword',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 210,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut5',
+		class: 'sword',
+		name: 'Arcane Rapier',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 142.5,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+
+			{
+				damage: 300,
+				fireRate: 20,
+				shots: 1,
+				armorPiercing: true
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut6',
+		class: 'sword',
+		name: "Henchman's Claymore",
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 75,
+				fireRate: 110,
+				shots: 3,
+				armorPiercing: true
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut7',
+		class: 'sword',
+		name: 'Demon Blade',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 162.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut8',
+		class: 'sword',
+		name: 'Sword of Illumination',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 162.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut9',
+		class: 'sword',
+		name: 'Spectral Sword',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 112.5,
+				fireRate: 100,
+				shots: 2,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut10',
+		class: 'sword',
+		name: 'Ancient Stone Sword',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 375,
+				fireRate: 60,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut11',
+		class: 'sword',
+		name: "Pirate King's Cutlass",
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 205,
+				fireRate: 130,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut12',
+		class: 'sword',
+		name: 'Acidic Slasher',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 375,
+				fireRate: 60,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut13',
+		class: 'sword',
+		name: 'Gaseous Glaive',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 205,
+				fireRate: 130,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut14',
+		class: 'sword',
+		name: "Pirate's Artillery",
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 777,
+				fireRate: 25,
+				shots: 1,
+				armorPiercing: false
+			},
+
+			{
+				damage: 50,
+				fireRate: 25,
+				shots: 7,
+				armorPiercing: false
+			},
+
+			{
+				damage: 50,
+				fireRate: 25,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	{
+		id: 'sword_ut15',
+		class: 'sword',
+		name: 'Sword of the Colossus',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 287.5,
+				fireRate: 100,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	/*
+	{
+		id: 'sword_ut16',
+		class: 'sword',
+		name: 'Divinity',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 300,
+				fireRate: 60,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+	*/
+	{
+		id: 'sword_ut17',
+		class: 'sword',
+		name: 'Atavistic Soul Sabre',
+		tier: "UT",
+		projectiles: [
+			{
+				damage: 447.5,
+				fireRate: 60,
+				shots: 1,
+				armorPiercing: false
+			},
+		],
+		canEquip: ['warrior', 'knight', 'paladin'],
+	},
+];
